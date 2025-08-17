a626a994fadc6a9a605a6c999daf569b20137cc2
chintaman2004 <ahmed.hassanc@gmail.com>
2025-08-17 04:39:34 -0700

    Main.dart Error Fixed

--- stagged_app/lib/main.dart
+++ stagged_app/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
