80b1456547ef1051a357406657b62c0da7b89af7
chintaman2004 <ahmed.hassanc@gmail.com>
2025-08-17 03:36:13 -0700

    New Project Created

--- ewallet/android/app/src/main/kotlin/com/example/ewallet/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ewallet
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ stagged_app/android/app/src/main/kotlin/com/example/stagged_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stagged_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
